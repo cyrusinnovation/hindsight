0d8ad19844aa1c75b61f8a1a54f35bf1b4aa6296
Beth Brown <bbrown@cyrusinnovation.com>
2014-06-04 15:03:46 -0400

    Display the retrospective date

--- test/integration/create_retrospective.js
+++ test/integration/create_retrospective.js
@@ -1,17 +1,20 @@
 describe('create retrospective', function() {
-  it('should save the new retro name', function() {
+  it('should display the new retro name and date', function() {
     browser.get('#/retrospectives');
 
     var name = element(by.model('newRetro.name'));
     name.sendKeys('Team Retro numero uno');
 
     var date = element(by.model('newRetro.date'));
-    date.sendKeys('06/12/15');
+    date.sendKeys('06/12/15 2:00pm');
 
     $('#new-retro-submit').click();
 
     var retroName = element(by.binding('retro.name'));
     expect(retroName.getText()).toEqual('Team Retro numero uno');
+
+    var retroDate = element(by.binding('retro.date'));
+    expect(retroDate.getText()).toEqual('06/12/15 2:00pm');
 
   });
 });
