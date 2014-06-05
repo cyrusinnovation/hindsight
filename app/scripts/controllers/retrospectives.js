2e129bf25697953b1b347ff7878efe9fac8f06a3
Beth Brown <bbrown@cyrusinnovation.com>
2014-06-05 10:17:18 -0400

    Use html5 date picker and required functionality

--- app/scripts/controllers/retrospectives.js
+++ app/scripts/controllers/retrospectives.js
@@ -1,9 +1,12 @@
 'use strict';
 
 angular.module('hindsightApp')
-  .controller('RetrospectivesCtrl', function ($scope) {
+  .controller('RetrospectivesCtrl', function ($scope, $filter) {
     $scope.retros = [];
     $scope.newRetro = {name: '', date: ''};
+    var date = new Date();
+    $scope.minDate = $filter('date')(date, "yyyy-MM-dd");
+
 
     this.addRetro = function(){
       $scope.retros.push($scope.newRetro);
