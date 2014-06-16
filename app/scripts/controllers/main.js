1d714925c98a7bd7fb986326a443503e57b13b4c
Beth Brown <bbrown@cyrusinnovation.com>
2014-06-16 16:48:58 -0400

    Added nav highlighting for current page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-hindsightApp.controller('MainCtrl', function ($scope) {});
+hindsightApp.controller('MainCtrl', function ($scope, $location) {
+  $scope.getClass = function (path) {
+    if($location.path() == path) {
+      return "active";
+    } else {
+      return "";
+    }
+  };
+
+});
