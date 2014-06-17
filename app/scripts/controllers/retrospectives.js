c3d712962839e425f1034b128724ce99ef5f2ab9
Beth Brown <bbrown@cyrusinnovation.com>
2014-06-17 16:33:21 -0400

    Added a post to the server create retro

--- app/scripts/controllers/retrospectives.js
+++ app/scripts/controllers/retrospectives.js
@@ -1,11 +1,22 @@
 'use strict';
 
-hindsightApp.controller('RetrospectivesCtrl', function ($scope, $filter) {
+hindsightApp.controller('RetrospectivesCtrl', function ($scope, $filter, $http) {
   $scope.retros = [];
   $scope.newRetro = {name: '', date: ''};
   $scope.minDate = $filter('date')(new Date(), "yyyy-MM-dd");
 
   this.addRetro = function(){
+
+    $http({
+      method: 'POST',
+      url: 'http://localhost:3000/retrospective',
+      params: {
+        label: $scope.newRetro.name,
+        start: $scope.newRetro.date
+      },
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+    });
+
     $scope.retros.push($scope.newRetro);
     $scope.newRetro = {name: '', date: ''};
   };
